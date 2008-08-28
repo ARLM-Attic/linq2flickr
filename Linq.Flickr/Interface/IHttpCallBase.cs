8cc50c954c3dc58e9ab6ec7764dec40a98febe0b
mehfuzh <mehfuzh@localhost>
2008-08-28 09:09:05 +0000

    Updated the IHttpCallBase

--- Linq.Flickr/Interface/IHttpCallBase.cs
+++ Linq.Flickr/Interface/IHttpCallBase.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace Linq.Flickr.Interface
 {
     public interface IHttpCallBase
     {
-        XElement GetElement(string requestUrl);
-        XElement ParseElement(string response);
+        XmlElement GetElement(string requestUrl);
+        XmlElement ParseElement(string response);
         string DoHTTPPost(string requestUrl);
     }
 }
